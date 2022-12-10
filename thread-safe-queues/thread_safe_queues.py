6e5b9cb13420c3927944ef8fb19ef22c0ead9d5d
Irish Micole Cando <irishmicolecando@gmail.com>
2022-12-11 07:12:48 +0800

    Import needed module from rich

--- thread-safe-queues/thread_safe_queues.py
+++ thread-safe-queues/thread_safe_queues.py
@@ -6,6 +6,12 @@ from queue import LifoQueue, PriorityQueue, Queue
 from dataclasses import dataclass, field
 from enum import IntEnum
 
+from rich.align import Align
+from rich.columns import Columns
+from rich.console import Group
+from rich.live import Live
+from rich.panel import Panel
+
 QUEUE_TYPES = {
   "fifo": Queue,
   "lifo": LifoQueue,
