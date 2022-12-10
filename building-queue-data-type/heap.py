1ccbf1a89b18d152406162612f0a4d5b8ed19fd5
Irish Micole Cando <irishmicolecando@gmail.com>
2022-12-10 21:21:55 +0800

    Use heappop to remove the first element in fruits list

--- building-queue-data-type/heap.py
+++ building-queue-data-type/heap.py
@@ -2,8 +2,14 @@ from heapq import heappush, heappop
 
 fruits = []
 
+# pushing elements to fruits list 
 heappush(fruits, "orange")
 heappush(fruits, "apple")
 heappush(fruits, "banana")
+
+print(fruits)
+
+# removing the first element in fruits list
+print(heappop(fruits))
 
 print(fruits)
