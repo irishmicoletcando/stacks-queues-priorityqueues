0d8bc0edb1e27562fb5cb47fca0fb32ea748b7c2
Irish Micole Cando <irishmicolecando@gmail.com>
2022-12-10 19:36:50 +0800

    Add a new class iterable and transfer len and iter methods

--- building-queue-data-type/queues.py
+++ building-queue-data-type/queues.py
@@ -1,25 +1,16 @@
 from collections import deque
 
-class Queue:
-    def __init__(self):
-        self._elements = deque()
-
-    def enqueue(self, element):
-        self._elements.append(element)
-
-    def dequeue(self):
-        return self._elements.popleft()
-
-class Queue:
-    def __init__(self, *elements):
-      self._elements = deque(elements)
-
+class Iterable:
     def __len__(self):
       return len(self._elements)
 
     def __iter__(self):
       while len(self) > 0:
         yield self.dequeue()
+
+class Queue(Iterable):
+    def __init__(self, *elements):
+      self._elements = deque(elements)
 
     def enqueue(self, element):
       self._elements.append(element)
