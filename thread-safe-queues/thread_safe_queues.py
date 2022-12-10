36f46676c7f68752b0a1a2d4bb31657fead20c87
Irish Micole Cando <irishmicolecando@gmail.com>
2022-12-11 02:49:17 +0800

    Fix variable name of products in main function

--- thread-safe-queues/thread_safe_queues.py
+++ thread-safe-queues/thread_safe_queues.py
@@ -50,34 +50,6 @@ PRIORITIZED_PRODUCTS = (
     Product(Priority.LOW, ":3rd_place_medal:"),
 )
 
-def main(args):
-  buffer = QUEUE_TYPES[args.queue]()
-  producers = [
-        Producer(args.producer_speed, buffer, PRODUCTS)
-        for _ in range(args.producers)
-    ]
-  consumers = [
-      Consumer(args.consumer_speed, buffer) for _ in range(args.consumers)
-  ]
-
-  for producer in producers:
-      producer.start()
-
-  for consumer in consumers:
-      consumer.start()
-  
-  view = View(buffer, producers, consumers)
-  view.animate()
-
-def parse_args():
-  parser = argparse.ArgumentParser()
-  parser.add_argument("-q", "--queue", choices=QUEUE_TYPES, default="fifo")
-  parser.add_argument("-p", "--producers", type=int, default=3)
-  parser.add_argument("-c", "--consumers", type=int, default=2)
-  parser.add_argument("-ps", "--producer-speed", type=int, default=1)
-  parser.add_argument("-cs", "--consumer-speed", type=int, default=1)
-  return parser.parse_args()
-
 class Worker(threading.Thread):
     def __init__(self, speed, buffer):
         super().__init__(daemon=True)
@@ -127,6 +99,36 @@ class Consumer(Worker):
             self.simulate_work()
             self.buffer.task_done()
             self.simulate_idle()
+
+def main(args):
+  buffer = QUEUE_TYPES[args.queue]()
+  products = PRIORITIZED_PRODUCTS if args.queue == "heap" else PRODUCTS
+  producers = [
+        Producer(args.producer_speed, buffer, products)
+        for _ in range(args.producers)
+    ]
+  consumers = [
+      Consumer(args.consumer_speed, buffer) for _ in range(args.consumers)
+  ]
+
+  for producer in producers:
+      producer.start()
+
+  for consumer in consumers:
+      consumer.start()
+  
+  view = View(buffer, producers, consumers)
+  view.animate()
+
+def parse_args():
+  parser = argparse.ArgumentParser()
+  parser.add_argument("-q", "--queue", choices=QUEUE_TYPES, default="fifo")
+  parser.add_argument("-p", "--producers", type=int, default=3)
+  parser.add_argument("-c", "--consumers", type=int, default=2)
+  parser.add_argument("-ps", "--producer-speed", type=int, default=1)
+  parser.add_argument("-cs", "--consumer-speed", type=int, default=1)
+  return parser.parse_args()
+
 
 if __name__ == "__main__":
     try:
