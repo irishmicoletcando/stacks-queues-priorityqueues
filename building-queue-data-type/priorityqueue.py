db33be94d65eb9fa6b98fa9a55d949efc8653880
Irish Micole Cando <irishmicolecando@gmail.com>
2022-12-10 21:55:36 +0800

    Error since dequeue is printing the lowest priority

--- building-queue-data-type/priorityqueue.py
+++ building-queue-data-type/priorityqueue.py
@@ -4,4 +4,10 @@ CRITICAL = 3
 IMPORTANT = 2
 NEUTRAL = 1
 
-messages = PriorityQueue() 
+messages = PriorityQueue()
+messages.enqueue_with_priority(IMPORTANT, "Windshield wipers turned on")
+messages.enqueue_with_priority(NEUTRAL, "Radio station tuned in")
+messages.enqueue_with_priority(CRITICAL, "Brake pedal depressed")
+messages.enqueue_with_priority(IMPORTANT, "Hazard lights turned on")
+
+print(messages.dequeue())
