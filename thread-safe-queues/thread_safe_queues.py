197a88c401619a137023aa3b7cdec2f3da1eb8f1
Irish Micole Cando <irishmicolecando@gmail.com>
2022-12-11 01:26:37 +0800

    Picks a random delay in seconds for thread to continue

--- thread-safe-queues/thread_safe_queues.py
+++ thread-safe-queues/thread_safe_queues.py
@@ -62,6 +62,14 @@ class Worker(threading.Thread):
         self.progress = 0
         sleep(randint(1, 3))
 
+    def simulate_work(self):
+        self.working = True
+        self.progress = 0
+        delay = randint(1, 1 + 15 // self.speed)
+        for _ in range(100):
+            sleep(delay / 100)
+            self.progress += 1
+
 if __name__ == "__main__":
     try:
         main(parse_args())
