09526ac41e403b532a3f0a4518efeb01061bfb88
Irish Micole Cando <irishmicolecando@gmail.com>
2022-12-11 21:59:51 +0800

    Use fifo queue from queues.py

--- uk-roadmap/graph.py
+++ uk-roadmap/graph.py
@@ -33,3 +33,5 @@ def load_graph(filename, node_factory):
 def breadth_first_traverse(graph, source):
     queue = Queue(source)
     visited = {source}
+    while queue:
+        yield (node := queue.dequeue())
