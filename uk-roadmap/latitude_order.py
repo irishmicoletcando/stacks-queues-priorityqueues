88e54d836c548cd6fd79170a96b1a04a666e06be
Irish Micole Cando <irishmicolecando@gmail.com>
2022-12-14 07:13:20 +0800

    If-else statement to check if the year if between 1901 and 2000 and prints it

--- uk-roadmap/latitude_order.py
+++ uk-roadmap/latitude_order.py
@@ -13,4 +13,8 @@ nodes, graph = load_graph("roadmap.dot", City.from_dict)
 
 for node in nx.bfs_tree(graph, nodes["edinburgh"], sort_neighbors=order):
   print("📍", node.name)
-
+  if is_twentieth_century(node.year):
+    print("Found:", node.name, node.year)
+    break
+else:
+  print("Not found")
