9d879a88a289c37e4a018e23b6ab4ec11a5ebec6
Irish Micole Cando <irishmicolecando@gmail.com>
2022-12-14 02:28:14 +0800

    Loop in neighbors of London to get nodes

--- uk-roadmap/london_neighbors.py
+++ uk-roadmap/london_neighbors.py
@@ -2,3 +2,6 @@ from graph import City, load_graph
 import networkx as nx
 
 nodes, graph = load_graph("roadmap.dot", City.from_dict)
+
+for neighbor in graph.neighbors(nodes["london"]):
+  print(neighbor.name)
