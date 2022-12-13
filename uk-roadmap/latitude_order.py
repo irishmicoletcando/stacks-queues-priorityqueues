bf3cd415fb5341a4a8f7f0bde253e7c9409d36a6
Irish Micole Cando <irishmicolecando@gmail.com>
2022-12-14 07:09:36 +0800

    Checks if the year is between 1901 and 2000

--- uk-roadmap/latitude_order.py
+++ uk-roadmap/latitude_order.py
@@ -1,2 +1,10 @@
 import networkx as nx
 from graph import City, load_graph
+
+def order(neighbors):
+  def by_latitude(city):
+    return city.latitude
+  return iter(sorted(neighbors, key=by_latitude, reverse=True))
+
+def is_twentieth_century(year):
+  return year and 1901 <= year <= 2000
