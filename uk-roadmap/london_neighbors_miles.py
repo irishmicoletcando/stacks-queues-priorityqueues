ff3b6dcb4a792d9f96fd791633f5bfbb807f197f
Irish Micole Cando <irishmicolecando@gmail.com>
2022-12-14 02:38:51 +0800

    Define a function that takes neighbors and strategy as arguments which sorts items

--- uk-roadmap/london_neighbors_miles.py
+++ uk-roadmap/london_neighbors_miles.py
@@ -2,3 +2,6 @@ from graph import City, load_graph
 import networkx as nx
 
 nodes, graph = load_graph("roadmap.dot", City.from_dict)
+
+def sort_by(neighbors, strategy):
+  return sorted(neighbors.items(), key=lambda item: strategy(item[1]))
