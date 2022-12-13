cc70877eeb3a0d579b8c3eae4290090fd218d203
Irish Micole Cando <irishmicolecando@gmail.com>
2022-12-14 02:40:28 +0800

    Iterate London nodes to get their distance in miles and names

--- uk-roadmap/london_neighbors_miles.py
+++ uk-roadmap/london_neighbors_miles.py
@@ -8,3 +8,7 @@ def sort_by(neighbors, strategy):
 
 def by_distance(weights):
   return float(weights["distance"])
+
+
+for neighbor, weights in sort_by(graph[nodes["london"]], by_distance):
+  print(f"{weights['distance']:>3} miles, {neighbor.name}")
