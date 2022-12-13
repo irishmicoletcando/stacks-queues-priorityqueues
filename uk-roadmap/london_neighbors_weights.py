409e810d1f2d902419bae691c51f1eca3ae2278a
Irish Micole Cando <irishmicolecando@gmail.com>
2022-12-14 02:33:14 +0800

    Iterate in nodes of London items to get neighbors and their weights

--- uk-roadmap/london_neighbors_weights.py
+++ uk-roadmap/london_neighbors_weights.py
@@ -2,3 +2,6 @@ from graph import City, load_graph
 import networkx as nx
 
 nodes, graph = load_graph("roadmap.dot", City.from_dict)
+
+for neighbor, weights in graph[nodes["london"]].items():
+  print(weights["distance"], neighbor.name)
