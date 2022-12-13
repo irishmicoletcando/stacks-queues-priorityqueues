e45aad69ae7a1ab514ce1be088e7073204b4b81e
Irish Micole Cando <irishmicolecando@gmail.com>
2022-12-14 02:39:37 +0800

    Define a function that returns distance of London neighbors

--- uk-roadmap/london_neighbors_miles.py
+++ uk-roadmap/london_neighbors_miles.py
@@ -5,3 +5,6 @@ nodes, graph = load_graph("roadmap.dot", City.from_dict)
 
 def sort_by(neighbors, strategy):
   return sorted(neighbors.items(), key=lambda item: strategy(item[1]))
+
+def by_distance(weights):
+  return float(weights["distance"])
