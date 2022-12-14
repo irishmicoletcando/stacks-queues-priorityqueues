7490b44a2b8deb4195a8cf572981ee42c81a6261
Irish Micole Cando <irishmicolecando@gmail.com>
2022-12-14 08:12:28 +0800

    Checks if belfast and glasgow nodes are connected

--- uk-roadmap/connected_nodes.py
+++ uk-roadmap/connected_nodes.py
@@ -2,3 +2,5 @@ from graph import connected
 from graph import City, load_graph
 
 nodes, graph = load_graph("roadmap.dot", City.from_dict)
+
+print(connected(graph, nodes["belfast"], nodes["glasgow"]))
