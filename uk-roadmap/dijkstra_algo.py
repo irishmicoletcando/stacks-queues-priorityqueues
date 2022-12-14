5563b786514e379e4f3465cd03622f6b6be50dbb
Irish Micole Cando <irishmicolecando@gmail.com>
2022-12-14 08:44:35 +0800

    Declare variables of london and edinburgh nodes

--- uk-roadmap/dijkstra_algo.py
+++ uk-roadmap/dijkstra_algo.py
@@ -2,3 +2,6 @@ import networkx as nx
 from graph import City, load_graph, dijkstra_shortest_path
 
 nodes, graph = load_graph("roadmap.dot", City.from_dict)
+
+city1 = nodes["london"]
+city2 = nodes["edinburgh"]
