5437580741a3e39c85bddeade36a0ef0d9f8953d
Irish Micole Cando <irishmicolecando@gmail.com>
2022-12-14 08:20:21 +0800

    Iterate loop through edinburgh nodes

--- uk-roadmap/depth_first_search.py
+++ uk-roadmap/depth_first_search.py
@@ -5,3 +5,6 @@ def is_twentieth_century(year):
   return year and 1901 <= year <= 2000
 
 nodes, graph = load_graph("roadmap.dot", City.from_dict)
+
+for node in nx.dfs_tree(graph, nodes["edinburgh"]):
+  print("📍", node.name)
